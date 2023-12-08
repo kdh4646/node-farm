b8551c49224e70b1bed50e656d2bf4bcd8571430
Derek Kweon <kdh4646@hanmail.net>
2023-12-07 21:50:34 -0800

    2023-12-07 read and write file

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const fs = require("fs");
 
 const hello = "hello world";
 console.log(hello);
+
+//read files
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+//write files
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written!");
